9be1e5d85d461e6c4547065bf5e40a79aaa30cc9
PhilBug <filip.bugaj13@gmail.com>
2018-04-23 22:08:59 +0200

    problem with asp.net_mvc.models.song

--- zadMVC/Controllers/SongsController.cs
+++ zadMVC/Controllers/SongsController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using zadMVC.Models;
 
 namespace zadMVC.Controllers
 {
@@ -11,7 +12,8 @@ namespace zadMVC.Controllers
         // GET: Songs
         public ActionResult Index()
         {
-            return View();
+            var song = new Song("Chciałem być", "Krzysztof Krawczyk", "Ballada", 1);
+            return View(song);
             //return Content("Hello world");
         }
 
---
+++ zadMVC/Models/Song.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace zadMVC.Models
+{
+    public class Song
+    {
+        public string Name, Artist, Genre;
+        public int id;
+
+        public Song() { }
+        
+        public Song(string Name, string Artist, string Genre, int id)
+        {
+            this.Name = Name;
+            this.Artist = Artist;
+            this.Genre = Genre;
+            this.id = id;
+        }
+    }
+}
