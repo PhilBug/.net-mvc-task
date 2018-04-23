ecb8c129e9a1c1a6da33bd886630daf4c8061da8
PhilBug <filip.bugaj13@gmail.com>
2018-04-23 21:10:14 +0200

    initial commit

---
+++ zadMVC/Controllers/SongsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace zadMVC.Controllers
+{
+    public class SongsController : Controller
+    {
+        // GET: Songs
+        public ActionResult Index()
+        {
+            return View();
+            //return Content("Hello world");
+        }
+
+        public ActionResult Square(int number)
+        {
+            var message = Math.Pow(number, 2).ToString();
+            return Content(message);
+        }
+    }
+}
